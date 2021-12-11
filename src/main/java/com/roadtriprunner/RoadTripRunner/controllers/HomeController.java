ec5f1f229ff945c2d80fd44c91c83d901b95caf6
krystalwhite <krystalsnowhite@gmail.com>
2021-12-10 19:28:05 -0600

    repeated failed attempts at integrating api key environmental variable and src link; added controller

--- src/main/java/com/roadtriprunner/RoadTripRunner/controllers/HomeController.java
+++ src/main/java/com/roadtriprunner/RoadTripRunner/controllers/HomeController.java
@@ -1,14 +1,18 @@
-package com.roadtriprunner.RoadTripRunner.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class HomeController {
-
-    @GetMapping
-    public String index() {
-        return "index";
-    }
-
-}
+//package com.roadtriprunner.RoadTripRunner.controllers;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class HomeController {
+//
+//    @Value("${gmapsApiKey}")
+//    private String gmapsApiKey;
+//
+//    @GetMapping
+//    public String index() {
+//        return "index";
+//    }
+//
+//}
