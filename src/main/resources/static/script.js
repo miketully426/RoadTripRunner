ab4528a23a529160e43bf55bb1c9992b813e8620
krystalwhite <krystalsnowhite@gmail.com>
2021-12-17 15:12:36 -0600

    deleted simple map of Sydney; also deleted style.css file and merged with styles.css

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,13 +1,3 @@
-/*  Simple Map Display of Sydney
-var map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-} */
-
 function initMap() {
   const directionsService = new google.maps.DirectionsService();
   const directionsRenderer = new google.maps.DirectionsRenderer();
