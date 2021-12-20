b7e0ba5f09be1263307abf4380f305d4fd054129
krystalwhite <krystalsnowhite@gmail.com>
2021-12-20 12:31:33 -0600

    takes starting and ending locations and displays text driving directions between them

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -3,14 +3,20 @@ let centerLongitude = -87.65;
 let centerZoom = 7;
 
 function initMap() {
-  const directionsService = new google.maps.DirectionsService();
   const directionsRenderer = new google.maps.DirectionsRenderer();
+  const directionsService = new google.maps.DirectionsService();
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: centerZoom,
     center: { lat: centerLatitude, lng: centerLongitude },
+    disableDefaultUI: true,
   });
 
   directionsRenderer.setMap(map);
+  directionsRenderer.setPanel(document.getElementById("sidebar"));
+
+  const control = document.getElementById("floating-panel");
+
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
 
   const onChangeHandler = function () {
     calculateAndDisplayRoute(directionsService, directionsRenderer);
@@ -21,14 +27,13 @@ function initMap() {
 }
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  const start = document.getElementById("start").value;
+  const end = document.getElementById("end").value;
+
   directionsService
     .route({
-      origin: {
-        query: document.getElementById("start").value,
-      },
-      destination: {
-        query: document.getElementById("end").value,
-      },
+      origin: start,
+      destination: end,
       travelMode: google.maps.TravelMode.DRIVING,
     })
     .then((response) => {
@@ -36,4 +41,3 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
     })
     .catch((e) => window.alert("Directions request failed due to " + status));
 }
-
