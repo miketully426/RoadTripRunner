6a34da84d12a9487c2eadddb2055e57e7deee8c3
millylane003 <emilykgerst19@gmail.com>
2021-12-21 20:29:58 -0600

    added place search to script.js, is not generating markers at all

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,15 +1,16 @@
 let centerLatitude = 37.85;
 let centerLongitude = -97.65;
 let centerZoom = 5;
+let displayUSA;
 
 function initMap() {
   const directionsService = new google.maps.DirectionsService();
   const directionsRenderer = new google.maps.DirectionsRenderer();
+  const infoWindow = new google.maps.InfoWindow();
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: centerZoom,
     center: { lat: centerLatitude, lng: centerLongitude },
   });
-  const placesService = new google.maps.places.PlacesService(map);
 
   directionsRenderer.setMap(map);
 
@@ -19,16 +20,41 @@ function initMap() {
 
   document.getElementById("start").addEventListener("change", onChangeHandler);
   document.getElementById("end").addEventListener("change", onChangeHandler);
+
+//  http request searching for querytext of 'National Parks' and will return just the name
+//  let request = {
+//    query: "National Parks",
+//    fields: ["name", "geometry"]
+//  };
+
+   const placesService = new google.maps.places.PlacesService(map);
+
+   service = new google.maps.places.PlacesService(map);
+   service.findPlaceFromQuery(request, (results, status) => {
+     if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+       for (let i = 0; i < results.length; i++) {
+         createMarker(results[i]);
+       }
+       map.setCenter(results[0].geometry.location);
+     }
+   });
 }
 
-//const displayNationalParksRequest = {
-//    location: document.getElementById("map");
-//    types: [ park ]
-//}
 
-let https = require("https");
+function createMarker(place) {
+  if (!place.geometry || !place.geometry.location) return;
 
-let
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", () => {
+    infoWindow.setContent(place.name || "");
+    infoWindow.open(map);
+  });
+}
+
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
   directionsService
