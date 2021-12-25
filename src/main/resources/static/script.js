210967a1bf2b4505ee64f3e236ad5ba8a0a3c4de
krystalwhite <krystalsnowhite@gmail.com>
2021-12-25 15:45:01 -0600

    attempts to make a createMarker function, but it does not work

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -15,9 +15,9 @@ function initMap() {
 //  displayUSA = new google.maps.LtLng(37.85, -97);
   const infoWindow = new google.maps.InfoWindow();
     const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: centerZoom,
-    center: { lat: centerLatitude, lng: centerLongitude },
-  });
+        zoom: centerZoom,
+        center: { lat: centerLatitude, lng: centerLongitude },
+    });
 
   directionsRenderer.setMap(map);
 
@@ -29,7 +29,7 @@ function initMap() {
   document.getElementById("end").addEventListener("change", onChangeHandler);
 
 /*NOTES:
-1. either input OR query + fields works.
+1. From Emily: either input OR query + fields works. From Krystal: I didn't get the input feature to work with the info window.
 2. the search text needs to be inside double quotes AND single quotes so that it searches for the exact phrase
 Will we want the request to be a user selected query or will it only be national parks?
 */
@@ -45,21 +45,22 @@ Will we want the request to be a user selected query or will it only be national
 /*NOTES:
 1. Tried creating createMarker function that takes in JSONObject as parameter and then loops through the jsonObject array to place markers.
 This didn't work; it loaded the map but had no markers within it. Also got two "user denied access to database" errors.
+However, the console.log did show that the JSON is being passed into the console, so the error is in the code rendering.
 
-2.  Try MarkerClusterer to attempt more marker placement.
+2.  ??? Try MarkerClusterer to attempt more marker placement???
 https://stackoverflow.com/questions/3059044/google-maps-js-api-v3-simple-multiple-marker-example
 
-3. Tried to connect marker with json.name text when looping through and placing marker initially; current code does not work.
+3. SUCCESS! Connected marker with json.name text when looping through and placing marker initially as part of init() function.
 */
 
 //created jsonObject inside and outside of
    service = new google.maps.places.PlacesService(map);
-//   service.findPlaceFromQuery
+//   service.findPlaceFromQuery(request, (results, status) => {
    service.textSearch(request, (results, status) => {
      let jsonString = JSON.stringify(results);
      let jsonObject = JSON.parse(jsonString);
-//     console.log(jsonObject[0]);
      if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+        console.log(jsonObject[0]);  // use to see what output comes from API
 
 //        createMarker(jsonObject);
        for (let i = 0; i < jsonObject.length; i++) {
@@ -79,39 +80,31 @@ https://stackoverflow.com/questions/3059044/google-maps-js-api-v3-simple-multipl
                 });
            }
        }
-
-        //call window function to display query
-//       }
-//       map.setCenter(jsonObject[0].geometry.location);
-//     }
-
-//      }
-
    });
 }
 
-
-function createMarker(jsonObject) {
-//  if (!place.geometry || !place.geometry.location) {
-//    return;
-//  }
+function createMarker(json) {
 
 //  const marker = new google.maps.Marker({
 //    map: map,
 //    position: place.geometry.location,
 //  });
-    for (let i = 0; i < jsonObject.length; i++) {
-           const marker = new google.maps.Marker({
+    for (let i = 0; i < json.length; i++) {
+        const marker = new google.maps.Marker({
              map: map,
-             position: jsonObject[i].geometry.location,
-           });
+             position: json[i].geometry.location,
+             title: json[i].name,
+        });
+
+        let infoWindowDisplayText = "<h1>National Park</h1>";
+
+        marker.addListener("click", () => {
+            infoWindow.setContent(json[i].name || infoWindowDisplayText);
+                infoWindow.open({
+                    anchor: marker,
+                    map });
+        });
     }
-
-
-//   google.maps.event.addListener(marker, "click", () => {
-//     infoWindow.setContent(place.name || "");
-//     infoWindow.open(map);
-//     });
 }
 
 
