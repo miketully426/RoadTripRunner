ae455bb5fd9e06f8bb755a52f2c07e268cd3b2cc
Melanie Gunn <mgunn589@gmail.com>
2021-12-27 15:18:23 -0600

    still playing around with autocomplete

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,6 +1,8 @@
+var map;
 let centerLatitude = 41.85;
 let centerLongitude = -87.65;
 let centerZoom = 7;
+let autocomplete;
 
 function initMap() {
   const directionsService = new google.maps.DirectionsService();
@@ -18,6 +20,13 @@ function initMap() {
 
   document.getElementById("start").addEventListener("change", onChangeHandler);
   document.getElementById("end").addEventListener("change", onChangeHandler);
+
+  const onPlaceChanged = function (){
+    initStartAndEndDestinationAutocomplete ();
+  }
+
+  document.getElementById("start_autocomplete").addEventListener("placeChanged", onPlaceChanged);
+  document.getElementById("end_autocomplete").addEventListener("placeChanged", onPlaceChanged)
 }
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
@@ -37,3 +46,29 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
     .catch((e) => window.alert("Directions request failed due to " + status));
 }
 
+
+//function initStartAndEndDestinationAutocomplete() {
+//    //everytime you autocomplete some thing you get a place object
+//    //takes in two parameters- 1.  id element from form  2.  object that takes options for autocomplete
+//    autocomplete = new google.maps.places.Autocomplete (
+//        document.getElementById('autocomplete'),
+//        {
+//            types: ['national park', 'address'],
+//            //component restrictions restricts search to a specific  country and makes search easier
+//            componentRestrictions: { 'country' : ['us']},
+//            //geometry- geolocation of your house, properly fomatted address, etc.
+//            fields: ['place_id', 'geometry', 'name']
+//        });
+
+//
+//}
+
+var options = {
+    types: ['(cities']
+}
+
+var input1 = document.getElementByID("start_autocomplete");
+var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+
+var input2 = document.getElementByID("end_autocomplete");
+var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
