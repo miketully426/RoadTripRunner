ad99bfca82254952280e3a8151a1e8d2b51fff58
krystalwhite <krystalsnowhite@gmail.com>
2021-12-30 16:19:36 -0600

    added in authentication filter and related code

---
+++ src/main/java/com/roadtriprunner/RoadTripRunner/JavaWebDevRoadtripRunnerAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.roadtriprunner.RoadTripRunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebDevRoadtripRunnerAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebDevRoadtripRunnerAuthenticationApplication.class, args);
+    }
+}
