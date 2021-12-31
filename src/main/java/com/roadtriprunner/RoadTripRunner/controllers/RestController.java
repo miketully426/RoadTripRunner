e9517e935148470c064f7cd4ff7e41c0fbda99a9
millylane003 <emilykgerst19@gmail.com>
2021-12-31 09:18:44 -0600

    added map to index to try and render directions on the client side

--- src/main/java/com/roadtriprunner/RoadTripRunner/controllers/RestController.java
+++ src/main/java/com/roadtriprunner/RoadTripRunner/controllers/RestController.java
@@ -1,26 +1,26 @@
-package com.roadtriprunner.RoadTripRunner.controllers;
-
-import com.roadtriprunner.RoadTripRunner.models.Location;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-@org.springframework.web.bind.annotation.RestController
-public class RestController {
-
-    @RequestMapping(value = "/maps/directions", method = "POST")
-    public String addOriginLocation (Location location, Model model) {
-        Location location = new Location();
-    }
-
-//example request to get JSON from server.. might have to be js to work?
-//const xmlhttp = new XMLHttpRequest();
-//    xmlhttp.onload = function() {
-//  const myObj = JSON.parse(this.responseText);
-//        document.getElementById("demo").innerHTML = myObj.name;
-//    };
-//xmlhttp.open("GET", "json_demo.txt");
-//xmlhttp.send();
-
-
-
-}
+//package com.roadtriprunner.RoadTripRunner.controllers;
+//
+//import com.roadtriprunner.RoadTripRunner.models.Location;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//@org.springframework.web.bind.annotation.RestController
+//public class RestController {
+//
+//    @RequestMapping(value = "/maps/directions", method = "POST")
+//    public String addOriginLocation (Location location, Model model) {
+//        Location location = new Location();
+//    }
+//
+////example request to get JSON from server.. might have to be js to work?
+////const xmlhttp = new XMLHttpRequest();
+////    xmlhttp.onload = function() {
+////  const myObj = JSON.parse(this.responseText);
+////        document.getElementById("demo").innerHTML = myObj.name;
+////    };
+////xmlhttp.open("GET", "json_demo.txt");
+////xmlhttp.send();
+//
+//
+//
+//}
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -3,179 +3,223 @@ let centerLatitude = 37.85;
 let centerLongitude = -97.65;
 let centerZoom = 4;
 
-function getAutocompleteData() {
-
-//might eventually want to add placeId as a field... TBD
- let autocompleteRequest =
-  {
-   componentRestrictions: {'country': ['us']},
-   fields: ['geometry', 'name', 'formatted_address']
-  }
-
-   var originInput = document.getElementById("originInput");
-   var origin = new google.maps.places.Autocomplete(originInput, autocompleteRequest);
-   var destinationInput = document.getElementById("destinationInput");
-   var destination = new google.maps.places.Autocomplete(destinationInput, autocompleteRequest);
-   var originPlace;
-   var jsonAutocompleteOrigin;
-   var jsonAutoObjectOrigin;
-   var jsonAutocompleteDestination;
-   var jsonAutoObjectDestination;
 
 
-   //declaring origin as an extension of the MVC Object class. likely unnecessary, TEST!
-   origin.prototype = new google.maps.MVCObject();
+//
+//function getAutocompleteData() {
+
+    //might eventually want to add placeId as a field... TBD
+//    let autocompleteRequest =
+//    {
+//        componentRestrictions: {'country': ['us']},
+//        fields: ['geometry', 'name', 'formatted_address']
+//    }
+//
+//    var originInput = document.getElementById("originInput");
+//    var origin = new google.maps.places.Autocomplete(originInput, autocompleteRequest);
+//    var destinationInput = document.getElementById("destinationInput");
+//    var destination = new google.maps.places.Autocomplete(destinationInput, autocompleteRequest);
+//    var originPlace;
+//    var jsonAutocompleteOrigin;
+//    var jsonAutoObjectOrigin;
+//    var jsonAutocompleteDestination;
+//    var jsonAutoObjectDestination;
+
+
+    //declaring origin as an extension of the MVC Object class. likely unnecessary, TEST!
+//    origin.prototype = new google.maps.MVCObject();
 
 
 
-/*  - .addListener is a MVCObject method. It takes the event "place_changed", which is the
-      only option for the autocomplete object. (autocomplete extends MVCObject)
+    /*  - .addListener is a MVCObject method. It takes the event "place_changed", which is the
+            only option for the autocomplete object. (autocomplete extends MVCObject)
     - will need to add logic in here to bind jsonobject to model if user is logged in  */
 
-   origin.addListener("place_changed", () => {
-     const originSelected = origin.getPlace();
-
-     if (!originSelected.name || !originSelected.geometry) {
-       window.alert("Yikes! We can't process that location. Please try another");
-     }
-
-     jsonAutocompleteOrigin = JSON.stringify(originSelected);
-     jsonAutoObjectOrigin = JSON.parse(jsonAutocompleteOrigin);
-
-
-
-// HTTP REQUEST (I THINK THIS MIGHT BE AJAX TOO...)
- function sendJSON() {
-     let originRequest = new XMLHttpRequest();
-     originRequest.open("POST", localhost8080/??, true);
-     originRequest.setRequestHeader("Content-Type", "application/json");
-
-     //create state change callback?
-     originRequest.onreadystatechange = function () {
-       if (originRequest.readyState === 4 && originRequest.status === 200) {
-
-        //print received data from server
-         result.innerHTML = this.responseText;
-       }
-     };
-
-     //convert JSON data to string
-     var data = JSON.stringify({ /*object here*/ });
-
-     //send data with request
-     originRequest.send(data);
-
-//use post because we want to send to database and save it
-// include url localhost8080/api ... (will not be a view)
+//    origin.addListener("place_changed", () => {
+//    const originSelected = origin.getPlace();
+//
+//    if (!originSelected.name || !originSelected.geometry) {
+//        window.alert("Yikes! We can't process that location. Please try another");
+//    }
+//
+//    jsonAutocompleteOrigin = JSON.stringify(originSelected);
+//    jsonAutoObjectOrigin = JSON.parse(jsonAutocompleteOrigin);
 
 
 
-//USING AJAX
-//     $.ajax({
-//       type : "POST",
-//       url : "/maps/directions",
-//       contentType: "application/json",
-//       data: jsonAutoObjectOrigin
-//     });
+//        // HTTP REQUEST (I THINK THIS MIGHT BE AJAX TOO...)
+//        function sendJSON() {
+//            let originRequest = new XMLHttpRequest();
+//            originRequest.open("POST", localhost8080/??, true);
+//            originRequest.setRequestHeader("Content-Type", "application/json");
+//
+//     //create state change callback?
+//            originRequest.onreadystatechange = function () {
+//                if (originRequest.readyState === 4 && originRequest.status === 200) {
+//
+//            //print received data from server
+//                    result.innerHTML = this.responseText;
+//                }
+//            };
+//
+//             //convert JSON data to string
+//            var data = JSON.stringify({ /*object here*/ });
+//
+//            //send data with request
+//            originRequest.send(data);
+//
+//            //use post because we want to send to database and save it
+//            // include url localhost8080/api ... (will not be a view)
 
 
 
-     console.log(jsonAutoObjectOrigin) //eventually take this out
-
-   });
-
-   destination.addListener("place_changed", () => {
-     const destinationSelected = destination.getPlace();
-
-     if (!destinationSelected.name || !destinationSelected.geometry) {
-       window.alert("Yikes! We can't process that location. Please try another.");
-     }
-
-     const jsonAutocompleteDestination = JSON.stringify(destinationSelected);
-     const jsonAutoObjectDestination = JSON.parse(jsonAutocompleteDestination);
-     console.log(jsonAutoObjectDestination); //eventually take this out
-   });
-
-}
+        //USING AJAX
+        //     $.ajax({
+        //       type : "POST",
+        //       url : "/maps/directions",
+        //       contentType: "application/json",
+        //       data: jsonAutoObjectOrigin
+        //     });
 
 
 
+//    console.log(jsonAutoObjectOrigin) //eventually take this out
+//
+//    });
+//
+//    destination.addListener("place_changed", () => {
+//        const destinationSelected = destination.getPlace();
+//
+//        if (!destinationSelected.name || !destinationSelected.geometry) {
+//            window.alert("Yikes! We can't process that location. Please try another.");
+//        }
+//
+//        const jsonAutocompleteDestination = JSON.stringify(destinationSelected);
+//        const jsonAutoObjectDestination = JSON.parse(jsonAutocompleteDestination);
+//        console.log(jsonAutoObjectDestination); //eventually take this out
+//    });
+
+//}
 
 
 function initMap() {
-  const directionsService = new google.maps.DirectionsService();
-  const directionsRenderer = new google.maps.DirectionsRenderer();
-  const infoWindow = new google.maps.InfoWindow();
 
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: centerZoom,
-    center: { lat: centerLatitude, lng: centerLongitude },
-  });
+    let autocompleteRequest =
+    {
+        componentRestrictions: {'country': ['us']},
+        fields: ['geometry', 'name', 'formatted_address']
+    }
+
+    var originInput = document.getElementById("originInput");
+    var origin = new google.maps.places.Autocomplete(originInput, autocompleteRequest);
+    var destinationInput = document.getElementById("destinationInput");
+    var destination = new google.maps.places.Autocomplete(destinationInput, autocompleteRequest);
+    var originPlace;
+    var jsonAutocompleteOrigin;
+    var jsonAutoObjectOrigin;
+    var jsonAutocompleteDestination;
+    var jsonAutoObjectDestination;
+
+    origin.addListener("place_changed", () => {
+        const originSelected = origin.getPlace();
+
+        if (!originSelected.name || !originSelected.geometry) {
+            window.alert("Yikes! We can't process that location. Please try another");
+        }
+
+        jsonAutocompleteOrigin = JSON.stringify(originSelected);
+        jsonAutoObjectOrigin = JSON.parse(jsonAutocompleteOrigin);
+
+        console.log(jsonAutoObjectOrigin) //eventually take this out
+
+    });
+
+    destination.addListener("place_changed", () => {
+        const destinationSelected = destination.getPlace();
+
+        if (!destinationSelected.name || !destinationSelected.geometry) {
+            window.alert("Yikes! We can't process that location. Please try another.");
+        }
+
+        const jsonAutocompleteDestination = JSON.stringify(destinationSelected);
+        const jsonAutoObjectDestination = JSON.parse(jsonAutocompleteDestination);
+        console.log(jsonAutoObjectDestination); //eventually take this out
+    });
 
 
 
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    const infoWindow = new google.maps.InfoWindow();
 
-  directionsRenderer.setMap(map);
-
-  const onChangeHandler = function () {
-    calculateAndDisplayRoute(directionsService, directionsRenderer);
-  };
-
-  document.getElementById("start").addEventListener("change", onChangeHandler);
-  document.getElementById("end").addEventListener("change", onChangeHandler);
-
-  let request = {
-    query: "'US national park'",
-  };
-
-//   const placesService = new google.maps.places.PlacesService(map);
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: centerZoom,
+        center: { lat: centerLatitude, lng: centerLongitude },
+    });
 
 
-   service = new google.maps.places.PlacesService(map);
-   service.textSearch(request, (results, status) => {
-     let jsonString = JSON.stringify(results);
-     let jsonObject = JSON.parse(jsonString);
-     if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-     console.log(jsonObject[10]);
+    directionsRenderer.setMap(map);
 
-       for (let i = 0; i < jsonObject.length; i++) {
-           const marker = new google.maps.Marker({
-             map: map,
-             position: jsonObject[i].geometry.location,
-             title: jsonObject[i].name,
-           });
+    const onChangeHandler = function () {
+        calculateAndDisplayRoute(directionsService, directionsRenderer);
+    };
 
-           let infoWindowDefaultText = "National Park";
-           let infoWindowMarkerText = "<b>"+`${jsonObject[i].name}`+"</b>" + "<br>" + `${jsonObject[i].formatted_address}` + "<br>" + `User Rating: ${jsonObject[i].rating}`;
+//    originSelected.addEventListener("change", onChangeHandler);
+//    destinationSelected.addEventListener("change", onChangeHandler);
 
-           marker.addListener("click", () => {
-             infoWindow.setContent(infoWindowMarkerText || infoWindowDefaultText);
-             infoWindow.open({
-                anchor: marker,
-                map });
-           });
-       }
-     }
-   });
+    let request = {
+        query: "'US national park'",
+    };
+
+    //   const placesService = new google.maps.places.PlacesService(map);
+
+
+    service = new google.maps.places.PlacesService(map);
+    service.textSearch(request, (results, status) => {
+        let jsonString = JSON.stringify(results);
+        let jsonObject = JSON.parse(jsonString);
+        if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+            console.log(jsonObject[10]);
+
+            for (let i = 0; i < jsonObject.length; i++) {
+                const marker = new google.maps.Marker({
+                    map: map,
+                    position: jsonObject[i].geometry.location,
+                    title: jsonObject[i].name,
+                });
+
+                let infoWindowDefaultText = "National Park";
+                let infoWindowMarkerText = "<b>"+`${jsonObject[i].name}`+"</b>" + "<br>" + `${jsonObject[i].formatted_address}` + "<br>" + `User Rating: ${jsonObject[i].rating}`;
+
+                marker.addListener("click", () => {
+                    infoWindow.setContent(infoWindowMarkerText || infoWindowDefaultText);
+                    infoWindow.open({
+                        anchor: marker,
+                        map });
+                });
+            }
+        }
+    });
+
+
+    function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+        directionsService
+        .route({
+            origin: {
+            query: originSelected.value,
+            },
+        destination: {
+            query: destinationSelected.value,
+        },
+        travelMode: google.maps.TravelMode.DRIVING,
+        })
+        .then((response) => {
+            directionsRenderer.setDirections(response);
+        })
+       .catch((e) => window.alert("Directions request failed due to " + status));
+    }
 }
 
 
 
 
-
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-  directionsService
-    .route({
-      origin: {
-        query: originInput.value,
-      },
-      destination: {
-        query: originInput.value,
-      },
-      travelMode: google.maps.TravelMode.DRIVING,
-    })
-    .then((response) => {
-      directionsRenderer.setDirections(response);
-    })
-    .catch((e) => window.alert("Directions request failed due to " + status));
-}
