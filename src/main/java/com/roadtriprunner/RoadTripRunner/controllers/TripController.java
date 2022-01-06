63cbe711a453d0f46644bc0a012bc44882e4e6a1
millylane003 <emilykgerst19@gmail.com>
2022-01-06 15:31:59 -0600

    updated TripController to reflect navbar_logout branch

--- src/main/java/com/roadtriprunner/RoadTripRunner/controllers/TripController.java
+++ src/main/java/com/roadtriprunner/RoadTripRunner/controllers/TripController.java
@@ -2,7 +2,9 @@ package com.roadtriprunner.RoadTripRunner.controllers;
 
 
 import com.roadtriprunner.RoadTripRunner.data.TripRepository;
+import com.roadtriprunner.RoadTripRunner.data.UserRepository;
 import com.roadtriprunner.RoadTripRunner.models.Trip;
+import com.roadtriprunner.RoadTripRunner.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @Controller
@@ -19,16 +24,36 @@ public class TripController {
     @Autowired
     TripRepository tripRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(AuthenticationController.userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
 
     @GetMapping("")
-    public String displayAllTrips(Model model) {
+    public String displayAllTrips(Model model, HttpServletRequest request) {
             model.addAttribute("title", "Your saved trips");
             model.addAttribute("trips", tripRepository.findAll());
+            User theUser = getUserFromSession(request.getSession());
+            model.addAttribute("loggedInUser", theUser);
         return "trips/index";
     }
 
     @GetMapping("/trip")
-    public String displayASingleTrip(@RequestParam Integer tripId, Model model) {
+    public String displayASingleTrip(@RequestParam Integer tripId, Model model, HttpServletRequest request) {
 
         Optional<Trip> possibleTrip = tripRepository.findById(tripId);
 
@@ -39,6 +64,7 @@ public class TripController {
             Trip trip = possibleTrip.get();
             model.addAttribute("title", trip.getTripName());
             model.addAttribute("trip", trip);
+            User theUser = getUserFromSession(request.getSession());
         }
         return "trips/trip";
     }
