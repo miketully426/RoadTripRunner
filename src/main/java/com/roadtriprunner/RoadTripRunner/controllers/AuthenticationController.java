d5df39a985a0cf854bf8ddd8749b875cb0d53216
millylane003 <emilykgerst19@gmail.com>
2022-01-06 15:22:23 -0600

    updated AuthenticationController to match navbar_logout branch

--- src/main/java/com/roadtriprunner/RoadTripRunner/controllers/AuthenticationController.java
+++ src/main/java/com/roadtriprunner/RoadTripRunner/controllers/AuthenticationController.java
@@ -9,13 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-
+import javax.script.ScriptException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -24,7 +24,7 @@ public class AuthenticationController {
     @Autowired
     UserRepository userRepository;
 
-    private static final String userSessionKey = "user";
+    public static final String userSessionKey = "user";
 
     public User getUserFromSession(HttpSession session) {
         Integer userId = (Integer) session.getAttribute(userSessionKey);
@@ -46,7 +46,7 @@ public class AuthenticationController {
     }
 
     @GetMapping("/register")
-    public String displayRegistrationForm(Model model) {
+    public String displayRegistrationForm(Model model, HttpServletRequest request) {
         model.addAttribute(new RegisterFormDTO());
         model.addAttribute("title", "Register");
         return "register";
@@ -87,7 +87,7 @@ public class AuthenticationController {
     }
 
     @GetMapping("/login")
-    public String renderLoginForm(Model model) {
+    public String renderLoginForm(Model model, User theUser) {
         model.addAttribute(new LoginFormDTO());
         model.addAttribute("title", "Login");
         return "login";
@@ -96,16 +96,16 @@ public class AuthenticationController {
     @PostMapping("/login")
     public String processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO,
                                    Errors errors, HttpServletRequest request,
-                                   Model model) {
+                                   Model model, User theUser) throws IOException, ScriptException, NoSuchMethodException {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Log In");
             return "login";
         }
 
-        User theUser = userRepository.findByUsername(loginFormDTO.getUsername());
+        User user = userRepository.findByUsername(loginFormDTO.getUsername());
 
-        if (theUser == null) {
+        if (user == null) {
             errors.rejectValue("username", "user.invalid", "The given username does not exist");
             model.addAttribute("title", "Log In");
             return "login";
@@ -119,13 +119,15 @@ public class AuthenticationController {
             return "login";
         }
 
-        setUserInSession(request.getSession(), theUser);
+        model.addAttribute("logout", "Logout");
+        setUserInSession(request.getSession(), user);
+        model.addAttribute("loggedInUser", user);
 
         return "redirect:/planATrip";
     }
 
     @GetMapping("/logout")
-    public String logout(HttpServletRequest request){
+    public String logout(HttpServletRequest request, Model model, User theUser){
         request.getSession().invalidate();
         return "redirect:/login";
     }
