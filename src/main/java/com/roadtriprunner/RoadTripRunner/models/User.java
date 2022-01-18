1c0f033d7f6ed8331fd1d425692c4faa08055c7b
millylane003 <emilykgerst19@gmail.com>
2022-01-18 12:06:18 -0600

    got rid of unused import statements

--- src/main/java/com/roadtriprunner/RoadTripRunner/models/User.java
+++ src/main/java/com/roadtriprunner/RoadTripRunner/models/User.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
